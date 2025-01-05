b62916ccc1f456cb639acfeb8c4565ac82614138
Isaacj3000 <Isaacjeanbaptiste95@gmail.com>
2025-01-05 18:53:48 -0500

    finnish my car controller file

--- controllers/car.js
+++ controllers/car.js
@@ -10,7 +10,6 @@ async function index(req, res) {
         res.status(500).send('Internal server error');
 
     }
-
 }
 function newCar(req, res) {
     res.render('cars/new', {title: 'new car'})
@@ -33,4 +32,63 @@ async function postCar(req, res) {
         res.status(500).send('Internal server error')
     }
 }
-module.exports = {index, newCar, postCar,}
+async function showCar(req, res) {
+    try {
+        const car = await Car.findbyId(req.params.id);
+    if (car) {
+        res.render('cars/show', { title: 'Car Details', car })
+    } else {
+        res.status(404).render('404/notFound', { title: " Car not found!" })
+        
+        }       
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('')
+    }
+}
+async function editCar(req, res) {
+    try {
+        const car = await Car.findById(req.params.id);
+        if (car) {
+            res.render('Car/edit', {title: 'Edit Car', car });
+        } else {
+            res.status(404).render('404/notFound', { title: 'Car not Found! '})
+        }
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Internal Server Error!')
+    }
+}
+async function updateCar(req, res) {
+    try {
+        const carId = parseInt(req.params.id);
+        const { id } = req.params;
+
+        const updatedCar = await Car.findByIdAndUpdate(id, req.body)
+        if (updatedCar) {
+            res.status(200).redirect('/cars');
+        } else {
+            res.status(400).render('404/notFound', { title: ' Car Not Found!'});
+        }
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Internal Server Error!')
+    }
+}
+async function deleteCar(req, res) {
+    try {
+        const { id } = req.params;
+        const deletedCar = await Car.findByIdAndDelete(id)
+        if (deletedCar) {
+            res.status(200).redirect(`/cars`)
+        } else {
+            res.status(400).render('404/notFound', { title: 'Car not Found!'});
+        }
+        res.redirect('/cars')
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error!")
+    }
+    res.redirect('/cars')
+}
+module.exports = { index, newCar, postCar, showCar, editCar, updateCar, deleteCar,}
